51d7cfda893b6120f9def4a52aa5805202fdf6ce
SandeepYadav99 <sandeepyadav.7one@gmail.com>
2023-08-12 14:10:38 +0530

    [fix] About

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -1,11 +1,11 @@
 import About from "@/pages/About-Us/About";
 import React from "react";
 
-// export const metadata = {
-//   title:
-//     "Welcome to TravelTippler – Your Journey Begins Here! - Travel Tippler",
-//   description: "Travel Tippler - ",
-// };
+export const metadata = {
+  title:
+    "Welcome to TravelTippler – Your Journey Begins Here! - Travel Tippler",
+  description: "Travel Tippler - ",
+};
 
 const AboutUs = () => {
   return (
